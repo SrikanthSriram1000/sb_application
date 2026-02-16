94ca73d06c9f36827a5666476da6733a115a985c
SrikanthSriram1000 <ssrikanth1000@gmail.com>
2026-02-16 23:38:51 +0530

    initial commit

---
+++ src/main/java/com/practice/logger/TestController.java
@@ -0,0 +1,19 @@
+package com.practice.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	
+@RequestMapping("/api")
+public class TestController {
+	Logger logger = LoggerFactory.getLogger(TestController.class);
+	@GetMapping("/hi")	
+	public String sayHai() {
+		logger.info("in hello method");
+		return "Hey ! heelo .";
+	}
+
+}
---
+++ src/test/java/com/practice/logger/LoggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
